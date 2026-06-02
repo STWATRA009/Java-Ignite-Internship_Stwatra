c64f85a93996522809b1c56bf6c2b42b4c71ad1f
rite2harini <rite2harini@gmail.com>
2026-06-02 14:38:36 +0530

    Create StudentProfile.java

---
+++ Day7/StudentProfile.java
@@ -0,0 +1,19 @@
+/*
+(EASY)
+Create a class Student:
+
+Requirements:
+name
+age
+marks
+Tasks:
+Initialize using constructor
+Create 2 student objects
+Display student details using method
+Twist:
+
+Marks should not be accessed directly.
+
+Write your code below
+----------------------------------------
+*/
